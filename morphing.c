9c013ec7734f7ebf49cd1f97574a957c030e5352
MHT <musab@MUSAB.localdomain>
2025-11-21 12:36:55 +0100

    Faire un bouton

--- morphing.c
+++ morphing.c
@@ -1,7 +1,13 @@
 #include "uvsqgraphics_2.h"
 #include "ChainedList.h"
 
-#define INTER_IMAGE 100
+#define INTER_IMAGE 950
+#define LARGEUR 1500
+#define HAUTEUR 900
+
+/*
+    Faire un bouton stop avec fonction qui renvoie 1 si appuyer
+*/
 
 struct pixel {
     int R, G, B;
@@ -11,10 +17,11 @@ struct image {
     int hauteur;
     int largeur;
     int max_value_rgb;
+    int decal_value;
     struct pixel **P;
 };
 
-void write_image(char *nom, struct image I) {
+void Write_Image(char *nom, struct image I) {
     FILE *F;
     F = fopen(nom, "w");
     if (!F) {perror("Pd de fichier"); exit(20);}
@@ -35,7 +42,7 @@ void write_image(char *nom, struct image I) {
     fclose(F);
 }
 
-struct image read_image(char *nom, struct image I) {
+struct image Read_Image(char *nom, struct image I) {
     FILE *F;
     F = fopen(nom, "r");
     if (!F) {printf("Pb de fichier"); exit(20);}
@@ -71,7 +78,7 @@ struct image read_image(char *nom, struct image I) {
     return I;
 }
 
-void show_image(struct image I, int decalh) {
+void Show_Image(struct image I) {
     set_mode_WINDOW();
     for (int y = 0; y < I.hauteur; y++) {
         for (int x = 0; x < I.largeur; x++) {
@@ -80,39 +87,90 @@ void show_image(struct image I, int decalh) {
                 I.P[y][x].G,
                 I.P[y][x].B
             );
-            POINT p = {decalh + x, y};
+            POINT p = {I.decal_value + x, y};
             draw_pixel(p, c);
         }
     }
 } 
 
-LISTE_POINTS * Get_Pixel_Couple(LISTE_POINTS * Head, struct image I, struct image I2) {
-    
-    POINT p1 = wait_clic();
-    POINT p2 = wait_clic();
-
-    // Vérifier si p1 est dans l'image de gauche
-    if ((p1.x <= I.largeur && p1.x >= 0) && (p1.y <= I.hauteur && p1.y >= 0)) {
-        // Vérifier si p2 est bien dans l'image de droite
-        if ((p2.x <= 1500 && p2.x >= (1500-I2.largeur)) && (p2.y <= I2.hauteur && p2.y >= 0)) {
-            Head = insert_first(Head, p1.x, p1.y, p2.x, p2.y);
-        } else {printf("choisir une autre image !\n");}
-        // Vérifier si p1 est dans l'image de droite
-    } else if ((p1.x <= 1500 && p1.x >= (1500-I2.largeur)) && (p1.y <= I2.hauteur && p1.y >= 0)) {
-        // Vérifier si p2 est bien dans l'image de gauche
-        if ((p2.x <= I.largeur && p2.x >= 0) && (p2.y <= I.hauteur && p2.y >= 0)) {
-            Head = insert_first(Head, p2.x, p2.y, p1.x, p1.y);
-        } else {printf("choisir une autre image !\n");}
+int Is_In_Image(struct image I, POINT p) {
+    if ((p.x >= I.decal_value && p.x <= I.largeur + I.decal_value) 
+    && (p.y >= 0 && p.y <= HAUTEUR)) {
+        return 1;
     } else {
-        printf("Le clic n'est pas dans une image !\n");
+        return 0;
     }
-    
+}
+
+LISTE_POINTS * Get_Pixel_Couple(LISTE_POINTS * Head, struct image I, struct image I2) {
+    while(1) {
+        POINT p1 = wait_clic();
+        POINT p2 = wait_clic();
+
+        // Vérifier si p1 est dans l'image de gauche
+        if (Is_In_Image(I, p1)) {
+            // Vérifier si p2 est bien dans l'image de droite
+            if (Is_In_Image(I2, p2)) {
+                Head = insert_first(Head, p1.x, p1.y, p2.x, p2.y);
+            } else {
+                printf("choisir une autre image !\n");
+                return Head;
+            }
+        
+        // Vérifier si p1 est dans l'image de droite
+        } else if (Is_In_Image(I2, p1)) {
+            // Vérifier si p2 est bien dans l'image de gauche
+            if (Is_In_Image(I, p2)) {
+                Head = insert_first(Head, p2.x, p2.y, p1.x, p1.y);
+            } else {
+                printf("choisir une autre image !\n");
+                return Head;
+            }
+        }
+    }
     return Head;
 }
 
-// faire fonction qui prend le nombre de couples voulu en utilisant la fonction Get_Pixel_Couple()
-LISTE_POINTS * Create_Couples_of_Base_Points(LISTE_POINTS * Head, struct image I, struct image I2) {
-    
+LISTE_POINTS * Init_With_Couples_of_Base_Points(LISTE_POINTS * Head, truct image I, struct image I2) {
+    POINT g1;
+    POINT d1;
+    g1.x = I.decal_value;
+    g1.y = 0;
+    d1.x = I2.decal_value;
+    d1.y = 0;
+    Head = insert_first(Head, g1.x, g1.y, d1.x, d1.y);
+
+    POINT g2;
+    POINT d2;
+    g2.x = I.largeur;
+    g2.y = 0;
+    d2.x = I2.largeur + I2.decal_value;
+    d2.y = 0;
+    Head = insert_first(Head, g2.x, g2.y, d2.x, d2.y);
+
+    POINT g3;
+    POINT d3;
+    g3.x = I.decal_value;
+    g3.y = I.hauteur;
+    d3.x = I2.decal_value;
+    d3.y = I2.hauteur;
+    Head = insert_first(Head, g3.x, g3.y, d3.x, d3.y);
+
+    POINT g4;
+    POINT d4;
+    g4.x = I.largeur;
+    g4.y = I.hauteur;
+    d4.x = I2.largeur + I2.decal_value;
+    d4.y = I2.hauteur;
+    Head = insert_first(Head, g4.x, g4.y, d4.x, d4.y);
+
+    return Head;
+}
+
+LISTE_POINTS * Create_Couples_of_Points(LISTE_POINTS * Head, struct image I, struct image I2) {
+    // initialiser avec les couples de base
+    Head = Init_With_Couples_of_Base_Points(Head, I, I2);
+
     Head = Get_Pixel_Couple(Head, I, I2);
     
     return Head;
@@ -123,25 +181,28 @@ int main() {
     struct image I;
     struct image I2;
 
+    I.decal_value = 0;
+    I2.decal_value = INTER_IMAGE;
+
     LISTE_POINTS * Head;
     Head = NULL;
 
     char nom[] = "chat_chien_1.ppm";
 
-    I = read_image(nom, I);
-    I2 = read_image("chat_chien_2.ppm", I2);
+    I = Read_Image(nom, I);
+    I2 = Read_Image("chat_chien_2.ppm", I2);
 
-    write_image("chat_chien_remake.ppm", I);
-    write_image("chat_chien_remake_2.ppm", I2);
+    Write_Image("chat_chien_remake.ppm", I);
+    Write_Image("chat_chien_remake_2.ppm", I2);
 
-    init_graphics(1500,900);
+    init_graphics(LARGEUR, HAUTEUR);
 
-    show_image(I, 0);
-    show_image(I2, (1500-I.largeur) + INTER_IMAGE);
+    Show_Image(I);
+    Show_Image(I2);
 
     affiche_all();
 
-    Head = Create_Couples_of_Base_Points(Head, I, I2);
+    Head = Create_Couples_of_Points(Head, I, I2);
     print_list(Head);
     wait_escape();
     
